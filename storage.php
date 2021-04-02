e0efdfc0df407114e4912916313aa1c8664db074
[kyakso96] <kyakso113@gmail.com>
2021-04-02 21:26:56 +0100

    third commit

--- storage.php
+++ storage.php
@@ -1,22 +1,29 @@
+<?php
+
 use Google\Cloud\Storage\StorageClient;
 
-function upload_object($bucketName, $objectName, $source)  //uploading objects
-{
-    $storage = new StorageClient();
-    $file = fopen($source, 'r');
-    $bucket = $storage->bucket($bucketName);
-    $object = $bucket->upload($file, [
-        'name' => $objectName
-    ]);
-    printf('Uploaded %s to gs://%s/%s' . PHP_EOL, basename($source), $bucketName, $objectName);
-}
+class storage
 
-function download_object($bucketName, $objectName, $destination) //downloading objects
 {
-    $storage = new StorageClient();
-    $bucket = $storage->bucket($bucketName);
-    $object = $bucket->object($objectName);
-    $object->downloadToFile($destination);
-    printf('Downloaded gs://%s/%s to %s' . PHP_EOL,
-        $bucketName, $objectName, basename($destination));
+
+    function upload_object($bucketName, $objectName, $source)  //uploading objects
+    {
+        $storage = new StorageClient();
+        $file = fopen($source, 'r');
+        $bucket = $storage->bucket($bucketName);
+        $object = $bucket->upload($file, [
+            'name' => $objectName
+        ]);
+     printf('Uploaded %s to gs://%s/%s' . PHP_EOL, basename($source), $bucketName, $objectName);
+    }
+
+    function download_object($bucketName, $objectName, $destination) //downloading objects
+    {
+        $storage = new StorageClient();
+        $bucket = $storage->bucket($bucketName);
+        $object = $bucket->object($objectName);
+        $object->downloadToFile($destination);
+        printf('Downloaded gs://%s/%s to %s' . PHP_EOL,
+            $bucketName, $objectName, basename($destination));
+    }
 }
