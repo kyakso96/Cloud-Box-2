f04e93f1496ca96f309ea3aa364575a02acd122d
[kyakso96] <kyakso113@gmail.com>
2021-04-05 22:29:12 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,35 @@
+<<<<<<< Updated upstream
+=======
+<?php
+   include("DBConnect.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $email = mysqli_real_escape_string($db,$_POST['email']);
+      $pass = mysqli_real_escape_string($db,$_POST['pass']); 
+      
+      $sql = "SELECT * FROM Register WHERE email = '$email' and pass = '$pass'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("email");
+         $_SESSION['login_user'] = $email;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
+>>>>>>> Stashed changes
 <html>
 <head>
       <title> User login and Registration </title>
