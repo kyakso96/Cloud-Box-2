5eec66b4728299611048fb0e8cd0730a5201e67b
ihsan2323 <Ihsan.Sekercileroglu@city.ac.uk>
2021-04-05 22:27:08 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,75 +1,45 @@
-<?php
-   include("DBConnect.php");
-   session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $email = mysqli_real_escape_string($db,$_POST['email']);
-      $pass = mysqli_real_escape_string($db,$_POST['pass']); 
-      
-      $sql = "SELECT id FROM admin WHERE email = '$email' and pass = '$pass'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         session_register("email");
-         $_SESSION['login_user'] = $email;
-         
-         header("location: welcome.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
-   }
-?>
 <html>
-   
-   <head>
-      <title>Login Page</title>
-      
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         .box {
-            border:#666666 solid 1px;
-         }
-      </style>
-      
-   </head>
-   
-   <body bgcolor = "#FFFFFF">
-	
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-				
-            <div style = "margin:30px">
-               
-               <form action = "" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" value = " login "/><br />
-               </form>
-               
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-					
-            </div>
-				
-         </div>
-			
-      </div>
+<head>
+      <title> User login and Registration </title>
+      <link rel="stylesheet" type="text/css"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
+</head>
+<body>
+
+<div class="container">
+      <div class="login-box">
+      <div class="row">
+      <div class="col-md-6">
+            <h2> Login here </h2)
+            <form action="validation.php" method="post">
+                  <div class="form-gorup">
+                        <label>username</label>
+                        <input type="text" name="user" class="form-control" required>
+                        </div>
+                  <div class="form-group">
+                        <label> password</label>
+                        <input type="password" name="password" class="form-control" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary"> Login </button>
+                  </form>
+            </div>
+            <div class="col-md-6">
+            <h2> Register here </h2)
+            <form action="registration.php" method="post">
+                  <div class="form-gorup">
+                        <label>username</label>
+                        <input type="text" name="user" class="form-control" required>
+                        </div>
+                  <div class="form-group">
+                        <label> password</label>
+                        <input type="password" name="password" class="form-control" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary"> Register </button>
+                  </form>
+            </div>
+
+            </div>
+            
    </body>
-</html>
+   </html>
